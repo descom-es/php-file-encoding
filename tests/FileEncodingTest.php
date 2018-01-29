21c34d83719084e9e767735e6bb86d4fcd55d0ec
Juan José Llorens <llorensjj@gmail.com>
2018-01-29 18:17:03 +0100

    Changes

---
+++ tests/FileEncodingTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Descom\File_Encoding\Test;
+use Descom\File_Encoding\FileEncoding;
+
+class FileEncodingTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        copy('files/test_iso88591.txt', 'iso88591.txt');
+        copy('files/test_utf8.txt', 'utf8.txt');
+    }
+
+    /** @test */
+    public function test_encoding_iso88591_as_UTF8()
+    {
+        $encoding = new FileEncoding();
+        $result = FileEncoding->encodeFile('iso88591.txt', 'UTF-8', 'UTF-8,ISO-8859-1,WINDOWS-1252');
+        $this->assertTrue($result);
+    }
+
+    /** @test */
+    public function test_encoding_UTF8_as_UTF8()
+    {
+        $encoding = new FileEncoding();
+        $result = FileEncoding->encodeFile('utf8.txt');
+        $this->assertTrue($result);
+    }
+
+    /** @test */
+    public function test_encoding_file_not_exists()
+    {
+        $encoding = new FileEncoding();
+        $result = FileEncoding->encodeFile('test.txt');
+        $this->assertTrue(!$result);
+    }
+}
