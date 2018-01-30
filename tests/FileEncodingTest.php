ca1f10a75b30358cebb98ec36023875b820e04bd
César Garcia <cesar@descom.es>
2018-01-30 13:01:22 +0100

    testing

--- tests/FileEncodingTest.php
+++ tests/FileEncodingTest.php
@@ -1,42 +1,56 @@
 <?php
 
-namespace Descom\FileEncoding\Test;
+namespace Descom\File\Test;
 
-use Descom\FileEncoding;
+use Descom\File\Encoding;
+use PHPUnit\Framework\TestCase;
 
 class FileEncodingTest extends TestCase
 {
     public function setUp()
     {
         parent::setUp();
-        copy('files/test_iso88591.txt', 'iso88591.txt');
-        copy('files/test_utf8.txt', 'utf8.txt');
+        copy(__DIR__.'/files/test_iso-8859-1.txt', __DIR__.'/files/iso88591.txt');
+        copy(__DIR__.'/files/test_utf8.txt', __DIR__.'/files/utf8.txt');
+    }
+
+    public function close()
+    {
+        unlink(__DIR__.'/files/iso88591.txt');
+        unlink(__DIR__.'/files/utf8.txt');
     }
 
     /** @test */
     public function test_encoding_iso88591_as_UTF8()
     {
-        $encoding = new FileEncoding();
-        $this->assertTrue($encoding->detectEncoding('iso88591.txt', 'UTF-8,ISO-8859-1,WINDOWS-1252') == 'ISO-8859-1');
-        $this->assertTrue($encoding->encodeFile('iso88591.txt', 'UTF-8', 'UTF-8,ISO-8859-1,WINDOWS-1252'));
-        $this->assertTrue($encoding->checkEncoding('iso88591.txt','UTF-8'));
+        $encoding = new Encoding();
+
+        $this->assertTrue($encoding->detectEncoding(__DIR__.'/files/iso88591.txt', 'UTF-8,ISO-8859-1,WINDOWS-1252') == 'ISO-8859-1');
+        $this->assertTrue($encoding->encodeFile(__DIR__.'/files/iso88591.txt', 'UTF-8', 'UTF-8,ISO-8859-1,WINDOWS-1252'));
+        $this->assertTrue($encoding->checkEncoding(__DIR__.'/files/iso88591.txt','UTF-8'));
+
+        $this->close();
     }
 
     /** @test */
     public function test_encoding_UTF8_as_UTF8()
     {
-        $encoding = new FileEncoding();
-        $this->assertTrue($encoding->detectEncoding('utf8.txt', 'UTF-8,ISO-8859-1,WINDOWS-1252') == 'UTF-8');
-        $this->assertTrue($result = $encoding->encodeFile('utf8.txt'));
-        $this->assertTrue($encoding->checkEncoding('utf8.txt','UTF-8'));
+        $encoding = new Encoding();
+        $this->assertTrue($encoding->detectEncoding('tests/files/utf8.txt', 'UTF-8,ISO-8859-1,WINDOWS-1252') == 'UTF-8');
+        $this->assertTrue($result = $encoding->encodeFile('tests/files/utf8.txt'));
+        $this->assertTrue($encoding->checkEncoding('tests/files/utf8.txt','UTF-8'));
+
+        $this->close();
     }
 
     /** @test */
     public function test_encoding_file_not_exists()
     {
-        $encoding = new FileEncoding();
-        $this->assertFalse($encoding->detectEncoding('test.txt', 'UTF-8,ISO-8859-1,WINDOWS-1252') == 'UTF-8');
-        $this->assertFalse($result = $encoding->encodeFile('test.txt'));
-        $this->assertFalse($encoding->checkEncoding('test.txt','UTF-8'));
+        $encoding = new Encoding();
+        $this->assertFalse($encoding->detectEncoding('tests/files/test.txt', 'UTF-8,ISO-8859-1,WINDOWS-1252') == 'UTF-8');
+        $this->assertFalse($result = $encoding->encodeFile('tests/files/test.txt'));
+        $this->assertFalse($encoding->checkEncoding('tests/files/test.txt','UTF-8'));
+
+        $this->close();
     }
 }
